e164933318305d13d86cf892c770c3b119a2c5e1
worst <jeremy.blackburn@gmail.com>
2010-04-25 20:45:46 -0400

    closing the telnet session after each send.

--- sgviz_client.rb
+++ sgviz_client.rb
@@ -52,10 +52,26 @@ File.open(filename) do |log|
       # match.captures.each {|m| puts m}
       from_node = match.captures[0]
       to_node = match.captures[1]
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "add_node #{peer_id} #{to_node}"
+      if !send_to.nil?
+        t.close
+      end
 
       # set the node to trusted if necessary
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "set_trusted #{peer_id} #{to_node}" if from_node == to_node
+      if !send_to.nil?
+        t.close
+      end
       #next
       puts "*"*20
     elsif !(match = r_add_edge.match(line)).nil?
@@ -72,7 +88,15 @@ File.open(filename) do |log|
       puts "tag: #{tag}"
       puts "weight: #{weight}"
 
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "add_edge #{peer_id} #{from_node} #{to_node} #{tag} #{weight}"
+      if !send_to.nil?
+        t.close
+      end
       # t.puts "update_edge #{peer_id} #{from_node} #{to_node} #{tag} #{weight}"
       puts "*"*20
     elsif !(match = r_update_edge.match(line)).nil?
@@ -83,7 +107,15 @@ File.open(filename) do |log|
       
       puts "*"*20
       puts "Updating edge"
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "update_edge #{peer_id} #{from_node} #{to_node} #{tag} #{weight}"
+      if !send_to.nil?
+        t.close
+      end
       puts "*"*20
     elsif !(match = r_visit.match(line)).nil?
       from_node = match.captures[0]
@@ -91,14 +123,30 @@ File.open(filename) do |log|
       
       puts "*"*20
       puts "Visiting node"
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "visit #{peer_id} #{from_node} #{to_node}"
+      if !send_to.nil?
+        t.close
+      end
       puts "*"*20
     elsif !(match = r_add_node2.match(line)).nil?
       from_node = match.captures[0]
       
       puts "*"*20
       puts "Adding node (non initializer)"
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "add_node #{peer_id} #{from_node}"
+      if !send_to.nil?
+        t.close
+      end
       puts "*"*20
     elsif !(match = r_add_edge2.match(line)).nil?
       from_node = match.captures[0]
@@ -108,7 +156,15 @@ File.open(filename) do |log|
       
       puts "*"*20
       puts "Adding edge (non initializer)"
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "add_edge #{peer_id} #{from_node} #{to_node} #{tag} #{weight}"
+      if !send_to.nil?
+        t.close
+      end
       puts "*"*20
     elsif !(match = r_update_edge2.match(line)).nil?
       from_node = match.captures[0]
@@ -118,18 +174,42 @@ File.open(filename) do |log|
       
       puts "*"*20
       puts "Updating edge (non initializer)"
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "update_edge #{peer_id} #{from_node} #{to_node} #{tag} #{weight}"
+      if !send_to.nil?
+        t.close
+      end
       puts "*"*20
     elsif !(match = r_acl_blocked.match(line)).nil?
       puts "*"*20
       puts "BLOOOOOOOOOOOCKKKKEDDD!!!"
       node = match.captures[0]
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "blocked #{peer_id} #{node}"
+      if !send_to.nil?
+        t.close
+      end
     elsif !(match = r_trust.match(line)).nil?
       puts "*"*20
       puts "Trust request"
       node = match.captures[0]
+      if !send_to.nil?
+        # It's not that pretty to keep a telnet session open the entire time, but
+        # whatever.
+        t = TCPSocket.new(send_to, 5204)
+      end
       t.puts "set_trusted #{peer_id} #{node}"
+      if !send_to.nil?
+        t.close
+      end
     end
   end
 end
